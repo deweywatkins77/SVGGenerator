05ea790698f86dd7ba79a93e7411c1e76596b851
Dewey Watkins <deweywatkins77@gmail.com>
2023-03-27 12:24:33 -0400

    added questions

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const path = require('path')
 const inquirer = require('inquirer')
 const shapes = require('./lib/shapes')
 const generateSVG = require('./lib/generateSVG')
+
+questions = [
+      {
+        type: 'input',
+        name: 'text',
+        message: 'What three letters would you like displayed',
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: "What color would you like your text to be? \n(Type in a color, or hexadecimal value",
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like for your logo?',
+        choices: ['Square', 'Triangle', 'Circle', 'Ellipse'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: "What color would you like your shape to be? \n(Type in a color, or hexadecimal value",
+      },
+]
